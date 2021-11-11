c67903de93eabd311e03db50743cf19c6bb5a6bd
Bhanu <bhanupartap789@gmail.com>
2021-11-11 23:56:07 +0530

    intersection observer api

--- index.js
+++ index.js
@@ -31,12 +31,131 @@ function checkSize(x) {
 checkSize(x);
 x.addListener(checkSize);
 
-var objects = new IntersectionObserver(entries =>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
-            entry.target.classList.add('clientdesign')
-        }
-    })
-})
 
-objects.observe( document.querySelector('li.logan'));
+var client_names = document.querySelectorAll('.logan');
+
+var client_names_options = {
+    root:null,
+    threshold:0,
+        
+}
+
+var observing = new IntersectionObserver(callBackFunc , client_names_options);
+
+client_names.forEach(ele=>{
+    observing.observe(ele);
+});
+
+function callBackFunc(entry){
+    entry.forEach(ment => {
+        if(ment.isIntersecting){
+            ment.target.classList.add('onlyscale');
+        }else{
+            ment.target.classList.remove('onlyscale');
+            
+        }
+    });
+}
+
+var client_heading = document.querySelector('.upper');
+
+var client_options = {
+    root:null,
+    threshold:0.3,
+    rootMargin:'-50px',
+}
+
+var observer = new IntersectionObserver(callBackFunction , client_options);
+
+observer.observe(client_heading);
+
+function callBackFunction(entries){
+    entries.forEach(element => {
+        if(element.isIntersecting){
+            
+            element.target.classList.add('removing');
+        }else{
+            element.target.classList.remove('removing');
+            
+        }
+    });
+}
+
+var client_heading = document.querySelector('.titling');
+
+var client_optioning = {
+    root:null,
+    threshold:0.3,
+    rootMargin:'-50px',
+}
+
+var observer = new IntersectionObserver(callBackFunctions , client_optioning);
+
+observer.observe(client_heading);
+
+function callBackFunctions(entries){
+    entries.forEach(element => {
+        if(element.isIntersecting){
+            
+            element.target.classList.add('clients_design');
+        }else{
+            element.target.classList.remove('clients_design');
+            
+        }
+    });
+}
+
+
+var client_checking = document.querySelector('.dsc');
+
+var client_optionings = {
+    root:null,
+    threshold:0.3,
+    rootMargin:'-50px',
+}
+
+var observent = new IntersectionObserver(callBackFunctioned , client_optionings);
+
+observent.observe(client_checking);
+
+function callBackFunctioned(entries){
+    entries.forEach(element => {
+        if(element.isIntersecting){
+            
+            element.target.classList.add('clients_design');
+        }else{
+            element.target.classList.remove('clients_design');
+            
+        }
+    });
+}
+
+var positioning = document.querySelector('.middlers');
+
+var positioning_options = {
+    root:null,
+    threshold:1.0,
+}
+
+var watching = new IntersectionObserver(positionFunction , positioning_options);
+
+watching.observe(positioning);
+
+var sideways = document.getElementsByClassName('chilling');
+
+function positionFunction(entries){
+    entries.forEach(element => {
+        if(element.isIntersecting){
+        
+            element.target.classList.add('addposition');
+        }else{
+            element.target.classList.remove('addposition');
+            
+        }
+    });
+}
+
+
+
+
+
